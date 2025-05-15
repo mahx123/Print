deb3105d6f10570ec9635300ca6540d6088516eb
mahx123 <38576859+mahx123@users.noreply.github.com>
2025-05-15 11:38:54 +0800

    初始化

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.rookie.printonline {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.rookie.printonline to javafx.fxml;
+    exports com.rookie.printonline;
+}
