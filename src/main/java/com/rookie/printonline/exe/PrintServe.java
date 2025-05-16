86d7ac6a0d9b37d886a89308312e4e647b37bbe6
mahx123 <38576859+mahx123@users.noreply.github.com>
2025-05-16 09:32:21 +0800

    打印程序的java类

---
+++ src/main/java/com/rookie/printonline/exe/PrintServe.java
@@ -0,0 +1,25 @@
+package com.rookie.printonline.exe;
+/**
+ *@Title: PrintServe.java
+ *@Package: com.rookie.printonline.exe
+ *@Description: TODO
+ *@Author: mahx 马怀啸
+ *@Email: 616107968@qq.com
+ *@Date: 2025/5/16 9:30
+ *@Version: V1.0.0
+ *@Copyright: 菜鸟
+ */
+public class PrintServe {
+
+
+    /**
+     * 获得所有打印机名称
+     * @return
+     */
+    public static String getAllPrint(){
+
+
+        return "";
+    }
+
+}
